513923c7bf16c17464bf7214a73dca634f366c6d
R@DAGAS <jcvizcarra2021@gogle.com>
2023-08-17 22:42:11 -0500

    ya estoy aprendiendo

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
@@ -9,15 +8,5 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifExpertApp />
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />git
-
   </React.StrictMode>,
 )
